a6ced665ffed121163e6449b03a47e7a2b7326c1
efff <efraespada@gmail.com>
2015-04-20 20:11:40 +0200

    jhgvjhgvmhv

--- proyectoMatematicas/src/proyectoMatematicas/juegos/MayorMenorIgual.java
+++ proyectoMatematicas/src/proyectoMatematicas/juegos/MayorMenorIgual.java
@@ -4,33 +4,39 @@ import java.util.Random;
 
 import proyectoMatematicas.Utilidades;
 
-public class MayorMenorIgual extends Juegos {
+public class MayorMenorIgual extends Juegos implements Jugable{
 
-	@Override
-	public void jugar() {
+	public static void main(String[] args) {
 		boolean continua = true;// variable para saber si desea jugar de nuevo o
 		// no
 		boolean pasa = true; // variable utilizada para saber si pasa de nivel o
+		// salir
+		boolean salir = false; // salir del juego
 		// no
 		int aciertos = 0;// numero de aciertos
 		int nivel = 1;// nivel en el que se encuentra el jugador
+		
+		int[] dificultad = {10,100,1000};
 
 		System.out.println("Bienvenido al juego matematico educativo");
-		System.out
-				.println("¡¡¡PORQUE LAS MATES NO TIENEN QUE SER ABURRIDAS!!!");
+		System.out.println("ï¿½ï¿½ï¿½PORQUE LAS MATES NO TIENEN QUE SER ABURRIDAS!!!");
 
 		do {
 			for (int i = 0; i < 10; i++) {
+				System.out.println("*********************************");
 				System.out.println("NIVEL " + nivel);
+				System.out.println("Prueba nï¿½mero " + (i+1) + ":");
 				System.out.println("Se muestran dos numeros");
-				double numero = Math.pow(nivel, 10);
-				Random randomGenerator = new Random();
-				double randomInt = randomGenerator.nextInt((int) numero);
-				double randomInt2 = randomGenerator.nextInt((int) numero);
+
+				
+				
+				int randomInt = (int)(Math.random() * dificultad[nivel-1] + 1);
+				int randomInt2 = (int)(Math.random() * dificultad[nivel-1] + 1);
+				
 				System.out.println("A." + randomInt);
 				System.out.println("B." + randomInt2);
 
-				System.out.println("¿Como son los siguientes numeros? ");
+				System.out.println("ï¿½Como son los siguientes numeros? ");
 				System.out.println("1. A>B");
 				System.out.println("2. A<B");
 				System.out.println("3. A=B");
@@ -40,49 +46,59 @@ public class MayorMenorIgual extends Juegos {
 
 				switch (opcion) {
 				case 1:
-					if (randomInt > randomInt2) {
-						pasa = true;
-					} else
-						pasa = false;
-
+					if (randomInt > randomInt2) pasa = true;
+					else pasa = false;
+					break;
 				case 2:
 					if (randomInt < randomInt2) {
 						pasa = true;
 					} else
 						pasa = false;
+					break;
 				case 3:
-					if (randomInt == randomInt2) {
+					if(randomInt == randomInt2){
 						pasa = true;
-					} else
+					}else
 						pasa = false;
-
+					break;
 				}
 
-				if (pasa) {
-					System.out
-							.println("Enhorabuena tu respuesta es correcta pasas");
+				if (pasa) { System.out.println("Enhorabuena tu respuesta es correcta pasas");
 					aciertos++;
 				} else {
-					System.out.println("Lo siente respuesta incorrecta");
+					System.out.println("Lo siento, respuesta incorrecta");
 
 				}
 			}
-			if (aciertos > 5) {
-				System.out.println("Enhorabuena a pasado al siguiente nivel");
-				nivel++;
-			} else
-				System.out.println("Lo siento no ha pasado el juego ");
-			System.out.println("¿Desea continuar?");
-			System.out.println("¿S/N?");
-			char respuesta = Utilidades.leerCaracter();
-			if (respuesta == 'S') {
-				continua = true;
-				nivel = 1;
-			} else
-				continua = false;
+			if (aciertos >= 5) {
+				if(nivel <= 2) {
+					System.out.println("Enhorabuena ha pasado al siguiente nivel");
+					nivel++;
+				} else System.out.println("Has ganado!!!");
+			} else System.out.println("Lo siento no ha pasado el juego ");
+			
+			if(nivel <= 2){
+				
+				System.out.println("ï¿½Desea continuar?");
+				System.out.println("ï¿½S/N?");
+				char respuesta = Utilidades.leerCaracter();
+				if (respuesta == 'S') {
+					continua = true;
+				} else {
+					continua = false;
+					salir = true;
+				}
+			} else {
+				System.out.println("Ha finalizado el juego, pajillero");
+				salir = true;
+			}
+		} while ((continua || nivel < 4) || salir);
 
-		} while ((continua || nivel < 3));
+	}
 
+	@Override
+	public void jugar() {
+		
 	}
 
 }
